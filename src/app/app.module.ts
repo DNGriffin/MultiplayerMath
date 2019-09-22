881bc8b7da774b59be05a3506fc5ba2a6383b4da
DNGriffin <31415269+DNGriffin@users.noreply.github.com>
2019-09-22 16:27:29 -0500

    firebase and angular installed

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+var firebaseConfig = {
+  apiKey: "AIzaSyBLfTk9x_GXo0frYc-eRwevOntUB2uew8k",
+  authDomain: "mulitplayermath.firebaseapp.com",
+  databaseURL: "https://mulitplayermath.firebaseio.com",
+  projectId: "mulitplayermath",
+  storageBucket: "",
+  messagingSenderId: "285834359487",
+  appId: "1:285834359487:web:da7be87103590429e14c0a"
+};
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule, // firestore
+    AngularFireAuthModule, // auth
+    AngularFireStorageModule // storage
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
