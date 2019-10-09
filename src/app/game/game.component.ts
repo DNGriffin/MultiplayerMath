d0c3a3d6030c3ef8faeecf2c4f60aa9da097d0ca
DNGriffin <31415269+DNGriffin@users.noreply.github.com>
2019-10-08 19:36:31 -0500

    game progress

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -22,10 +22,17 @@ var twoKey: Phaser.Key;
 var threeKey: Phaser.Key;
 var fourKey: Phaser.Key;
 
+var aKey: Phaser.Key;
+var dKey: Phaser.Key;
+
+
 var oneKeyIcon: Phaser.Sprite;
 var twoKeyIcon: Phaser.Sprite;
 var threeKeyIcon: Phaser.Sprite;
 var fourKeyIcon: Phaser.Sprite;
+
+var spaceship: Phaser.Sprite;
+
 
 var questionText: Phaser.Text;
 
@@ -34,6 +41,18 @@ var twoKeyText: Phaser.Text;
 var threeKeyText: Phaser.Text;
 var fourKeyText: Phaser.Text;
 var starfield;
+var asteroid1, asteroid2, asteroid3, asteroid4, asteroid5;
+
+var questions = [["What is 3x*3?", "9x", "6x", "9", "6"],
+["What is 6x/6?", "x", "6", "1", "0"],
+["What is 3(x+2x)", "9x", "9x^2", "6x", "9"],
+["What is 5x-2?", "5x-2", "5x", "3x", "3"],
+["What is (3x-3x)*3x?", "0", "9x", "27x", "9"]
+  // ["", "", "", "", ""],
+
+
+
+]
 
 function preload() {
   game.load.image("1key", "assets/games/1key.png");
@@ -41,9 +60,22 @@ function preload() {
   game.load.image("3key", "assets/games/3key.png");
   game.load.image("4key", "assets/games/4key.png");
   game.load.image('starfield', 'assets/games/starfield.png');
+  game.load.image('spaceship', 'assets/games/spaceship.png');
+  game.load.image('asteroid1', 'assets/games/asteroid1.png');
 
 }
+var asteroidGroup;
+
 function create() {
+  asteroid1 = game.add.sprite(game.world.randomX, -game.world.randomY, "asteroid1");
+  asteroid2 = game.add.sprite(game.world.randomX, -game.world.randomY, "asteroid1");
+  asteroid3 = game.add.sprite(game.world.randomX, -game.world.randomY, "asteroid1");
+  asteroid4 = game.add.sprite(game.world.randomX, -game.world.randomY, "asteroid1");
+  asteroid5 = game.add.sprite(game.world.randomX, -game.world.randomY, "asteroid1");
+
+  game.physics.startSystem(Phaser.Physics.ARCADE);
+  game.physics.arcade.gravity.y = 100;
+
   starfield = game.add.tileSprite(0, 0, game.width, game.height, 'starfield');
 
   oneKeyIcon = game.add.sprite(game.width * 0.2, game.height * 0.2, "1key");
@@ -51,9 +83,31 @@ function create() {
   threeKeyIcon = game.add.sprite(oneKeyIcon.x, twoKeyIcon.y + twoKeyIcon.height * 1.5, "3key");
   fourKeyIcon = game.add.sprite(oneKeyIcon.x, threeKeyIcon.y + threeKeyIcon.height * 1.5, "4key");
 
+  spaceship = game.add.sprite(game.width * 0.5, game.height * 0.8, "spaceship");
+  game.physics.enable(spaceship, Phaser.Physics.ARCADE);
+  spaceship.scale.set(2, 2);
+  spaceship.body.gravity.y = 0;
+
+
+  asteroidGroup = game.add.group();
+  asteroidGroup.add(asteroid1);
+  asteroidGroup.add(asteroid2);
+  asteroidGroup.add(asteroid3);
+  asteroidGroup.add(asteroid4);
+  asteroidGroup.add(asteroid5);
+  asteroidGroup.forEach(function (sprite) {
+    sprite.anchor.setTo(0.5);
+    game.physics.enable(sprite, Phaser.Physics.ARCADE);
+    sprite.body.angularVelocity = Math.random() * 200;
+    sprite.body.angularDrag = 3;
+    sprite.body.allowGravity = true;
+    var asteroidWidth = game.height * .10 + game.height * 0.15 * Math.random();
+    sprite.width = asteroidWidth;
+    sprite.height = asteroidWidth;
+  })
   questionText = game.add.text(game.world.centerX, game.height * 0.15, "What is 3x * 3?");
   questionText.anchor.set(0.5);
-  oneKeyText = game.add.text(game.width * 0.2+oneKeyIcon.width, game.height * 0.2, "9x");
+  oneKeyText = game.add.text(game.width * 0.2 + oneKeyIcon.width, game.height * 0.2, "9x");
   twoKeyText = game.add.text(oneKeyIcon.x + oneKeyIcon.width, oneKeyIcon.y + oneKeyIcon.height * 1.5, "3x");
   threeKeyText = game.add.text(oneKeyIcon.x + oneKeyIcon.width, twoKeyIcon.y + twoKeyIcon.height * 1.5, "4x");
   fourKeyText = game.add.text(oneKeyIcon.x + oneKeyIcon.width, threeKeyIcon.y + threeKeyIcon.height * 1.5, "9");
@@ -69,28 +123,97 @@ function create() {
   threeKey = game.input.keyboard.addKey(Phaser.Keyboard.THREE);
   fourKey = game.input.keyboard.addKey(Phaser.Keyboard.FOUR);
 
+  aKey = game.input.keyboard.addKey(Phaser.Keyboard.A);
+  dKey = game.input.keyboard.addKey(Phaser.Keyboard.D);
+
+}
+var canAnswer = true;
+function asteroidUpdate() {
+  asteroidGroup.forEach(function (sprite) {
+    if (sprite.y > sprite.height + game.height) {
+      sprite.y = -sprite.height;
+      sprite.body.angularVelocity = Math.random() * 200 - Math.random() * 200;
+      sprite.x = game.world.randomX;
+      var asteroidWidth = game.height * .10 + game.height * 0.15 * Math.random();
+      sprite.width = asteroidWidth;
+      sprite.height = asteroidWidth;
+      // createMissile();
+    }
+    if (sprite.body.velocity.y > 200) {
+      sprite.body.velocity.y = 200;
+    }
+  })
+}
+function answerQuestion(index) {
+  if (canAnswer) {
+    nextQuestion();
+    canAnswer = false;
+    setTimeout(() => {
+      canAnswer = true;
+    }, 2000);
+  }
+}
+
+var questionIndex = 0;
+var ans = "";
+var isLeftTurn = true;
+function nextQuestion() {
+  questionIndex++;
+  questionIndex = questionIndex % questions.length;
+  questionText.text = questions[questionIndex][0];
+  ans = questions[questionIndex][1];
+  var tempAnswers = [];
+  for (var i = 1; i < questions[questionIndex].length; i++) {
+    tempAnswers.push(questions[questionIndex][i]);
+  }
+  tempAnswers = tempAnswers.sort(() => Math.random() - 0.5);
+
+  oneKeyText.text = tempAnswers[0];
+  twoKeyText.text = tempAnswers[1];
+  threeKeyText.text = tempAnswers[2];
+  fourKeyText.text = tempAnswers[3];
+
 }
 function update() {
+  spaceship.y = game.height * 0.5;
+
+  asteroidUpdate();
   starfield.tilePosition.y += 1;
-  if(oneKey.isDown){
+  if (oneKey.isDown) {
     oneKeyIcon.alpha = 1;
-  }else{
+    answerQuestion(1);
+  } else {
     oneKeyIcon.alpha = 0.7;
   }
-  if(twoKey.isDown){
+  if (twoKey.isDown) {
     twoKeyIcon.alpha = 1;
-  }else{
+    answerQuestion(2);
+
+  } else {
     twoKeyIcon.alpha = 0.7;
   }
-  if(threeKey.isDown){
+  if (threeKey.isDown) {
+    answerQuestion(3);
+
     threeKeyIcon.alpha = 1;
-  }else{
+  } else {
     threeKeyIcon.alpha = 0.7;
   }
-  if(fourKey.isDown){
+  if (fourKey.isDown) {
+    answerQuestion(4);
+
     fourKeyIcon.alpha = 1;
-  }else{
+  } else {
     fourKeyIcon.alpha = 0.7;
+  }
+  if(aKey.isDown){
+    spaceship.body.velocity.x = -game.width * 0.3;
+  }
+  if(dKey.isDown){
+    spaceship.body.velocity.x = +game.width * 0.3;
+  }
+  if(!aKey.isDown && !dKey.isDown){
+    spaceship.body.velocity.x = 0;
   }
 
 }
