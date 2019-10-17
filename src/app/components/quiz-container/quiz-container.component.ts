1c05a451e9b2a87f4b04a485357730bd389db4a9
Coby Drexler <coby.drexler@wustl.edu>
2019-10-17 18:02:19 -0500

    injecting quiz title input

--- src/app/components/quiz-container/quiz-container.component.ts
+++ src/app/components/quiz-container/quiz-container.component.ts
@@ -9,6 +9,16 @@ export class QuizContainerComponent implements OnInit {
 
   constructor() { }
 
+  quizes: Array<string> = ["Addition", "Multiplication", "Exponents"]
+
+  private getQuizTitle(quizId: number): string {
+    if (quizId < this.quizes.length) {
+      return this.quizes[quizId]
+    } else {
+      return "Quiz not found"
+    }
+  }
+
   ngOnInit() {
   }
 
